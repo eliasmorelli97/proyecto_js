2d22e61eff885d2d6c381ef32c95c77903114790
Elias Morelli <ejmorelli97@gmail.com>
2022-07-19 21:35:20 -0300

    Primer desafio complementario

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let repetir = true;
+
+let num = prompt("Ingrese un número para ver sus tablas del 1 al 10")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(num + " x " + i + " = " + (num * i ));
+}
